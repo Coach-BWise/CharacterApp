e401d9aa6ea0f17a6c41522135c8541b6dfe8ce6
Coach-BWise <wisebry11@gmail.com>
2021-07-22 11:06:23 -0400

    spring initializer

---
+++ src/main/java/com/revature/characterapp/CharacterAppApplication.java
@@ -0,0 +1,13 @@
+package com.revature.characterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterAppApplication.class, args);
+    }
+
+}
